f179aeeaaef7e1413c205c1127aa372b1fb0a558
iamsivin <iamsivin@gmail.com>
2022-01-04 22:02:06 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import PunkList from "./components/PunkList";
+import Main from "./components/Main";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setselectedPunk] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(
+        "/assets?asset_contract_address=0xD8e6f1104925E9519f29de5A058BF906C5FBfeDA&order_direction=asc"
+      );
+      setPunkListData(result.data.assets);
+    };
+    return fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      {punkListData.length > 0 && (
+        <>
+          <Main selectedPunk={selectedPunk} punkListData={punkListData} />
+          <PunkList
+            punkListData={punkListData}
+            setselectedPunk={setselectedPunk}
+          />
+        </>
+      )}
     </div>
   );
 }
